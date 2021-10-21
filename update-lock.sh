b0ecbe37471066b858347bf7fb6210370cb4551b
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-21 20:57:14 +0530

    chore(root): script to update lock version after running lerna version

---
+++ update-lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+tag=v$(node -e "process.stdout.write(require('./lerna.json').version)");
+
+# Update the lock file here
+npm i --package-lock-only
+
+# Auto-tag and auto-commit like usual
+git commit --all -m ${tag}
+git tag -a ${tag} -m ${tag}
+git push --tags
+git push
