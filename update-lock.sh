b4f03c9ad9cff41a368a075683083980fb83f006
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-22 13:47:04 +0530

    fix(button123): styl

--- update-lock.sh
+++ update-lock.sh
@@ -3,7 +3,7 @@
 # Update the lock file here
 npm i --package-lock-only
 
-# Auto-tag and auto-commit like usual
-HUSKY_SKIP_HOOKS=1 git commit -am "chore(release): cleanup changelog and update lockfile"
+# Auto-commit like usual
+HUSKY_SKIP_HOOKS=1 git commit -am "chore(release): cleanup changelogs and update lockfile"
 HUSKY_SKIP_HOOKS=1  git push --tags
 HUSKY_SKIP_HOOKS=1  git push
