3b8f1663e530f547e8ef2c1674f68ed28d1220d6
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-22 11:55:48 +0530

    chore(root): release scripts changes

--- update-lock.sh
+++ update-lock.sh
@@ -4,6 +4,6 @@
 npm i --package-lock-only
 
 # Auto-tag and auto-commit like usual
-HUSKY_SKIP_HOOKS=1 git commit -am "chore(release): publish"
+HUSKY_SKIP_HOOKS=1 git commit --amend --no-edit
 HUSKY_SKIP_HOOKS=1  git push --tags
 HUSKY_SKIP_HOOKS=1  git push
