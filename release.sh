3e08ea3cb1b146d1cb6b592c26f6ded5919b9203
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-22 14:23:43 +0530

    chore(root): rename release script

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # Update the lock file here
 npm i --package-lock-only
 
-# Auto-commit like usual
+# Auto-commit files and push tags
 HUSKY_SKIP_HOOKS=1 git add .
 HUSKY_SKIP_HOOKS=1 git commit --amend --no-edit
 HUSKY_SKIP_HOOKS=1  git push --tags
