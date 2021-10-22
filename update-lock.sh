1a8a38c4e94e6f7e6020af44eda80b8a50acf757
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-22 13:47:34 +0530

    chore(release): publish
     - cr-mnr-core@3.1.0-monorepo.8
     - cr-mnr-datatable@2.1.0-monorepo.2

--- update-lock.sh
+++ update-lock.sh
@@ -4,6 +4,6 @@
 npm i --package-lock-only
 
 # Auto-commit like usual
-HUSKY_SKIP_HOOKS=1 git commit -am "chore(release): cleanup changelogs and update lockfile"
+HUSKY_SKIP_HOOKS=1 git commit --amend --no-edit
 HUSKY_SKIP_HOOKS=1  git push --tags
 HUSKY_SKIP_HOOKS=1  git push
