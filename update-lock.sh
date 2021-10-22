1b3cc2d7414957de30597ac4431b892c760876aa
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-22 13:57:32 +0530

    release script changes

--- update-lock.sh
+++ update-lock.sh
@@ -4,6 +4,7 @@
 npm i --package-lock-only
 
 # Auto-commit like usual
+HUSKY_SKIP_HOOKS=1 git add .
 HUSKY_SKIP_HOOKS=1 git commit --amend --no-edit
 HUSKY_SKIP_HOOKS=1  git push --tags
 HUSKY_SKIP_HOOKS=1  git push
