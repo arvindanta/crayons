42322ac1f06ee5d5556b0b7d4eb7284792d30699
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-26 09:39:37 +0530

    chore(release): update lock file and cleanup changelogs

--- release.sh
+++ release.sh
@@ -5,6 +5,6 @@ npm i --package-lock-only
 
 # Auto-commit files and push tags
 HUSKY_SKIP_HOOKS=1 git add .
-HUSKY_SKIP_HOOKS=1 git commit -am "chore(release): update lock file and cleanup changelogs"
+HUSKY_SKIP_HOOKS=1 git commit -m "chore(release): update lock file and cleanup changelogs"
 HUSKY_SKIP_HOOKS=1 git push 
 HUSKY_SKIP_HOOKS=1 git push --tags
