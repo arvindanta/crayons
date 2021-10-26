cf3b563dbadf8fb6698a6423c091d369ff3606f2
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-26 08:53:56 +0530

    chore(release): asd

--- release.sh
+++ release.sh
@@ -5,8 +5,6 @@ npm i --package-lock-only
 
 # Auto-commit files and push tags
 HUSKY_SKIP_HOOKS=1 git add .
-#HUSKY_SKIP_HOOKS=1 git commit --amend --no-edit
 HUSKY_SKIP_HOOKS=1 git commit -am "chore(release): update lock file and cleanup changelogs"
-#HUSKY_SKIP_HOOKS=1  git push --tags
-HUSKY_SKIP_HOOKS=1 git push && git push --tags
-#HUSKY_SKIP_HOOKS=1  git push origin monorepo
+HUSKY_SKIP_HOOKS=1 git push 
+HUSKY_SKIP_HOOKS=1 git push --tags
